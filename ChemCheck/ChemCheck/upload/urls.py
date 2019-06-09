eb97974d6e1e325c0bca364ba5185dc7103d865e
Chao Xu <xc184332574@gmail.com>
2019-06-09 16:48:41 -0400

    Delete urls.py

--- ChemCheck/ChemCheck/upload/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from upload import views
-
-urlpatterns = [
-    path('home/', views.Home.as_view(), name='home'),
-    path('upload/', views.upload, name='upload'),
-]
