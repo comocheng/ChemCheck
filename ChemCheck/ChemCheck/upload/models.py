cc84b5f45349dd6f1f6b55447eb95c29d53c140f
Chao Xu <xc184332574@gmail.com>
2019-06-09 16:48:55 -0400

    Delete models.py

--- ChemCheck/ChemCheck/upload/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-import datetime
-
-# Create your models here.
-
-class Chemkin(models.Model):
-    upload_file = models.FileField(upload_to='uploads/', max_length=100, blank=True)
-    timestamps = models.DateTimeField(auto_now_add=True)
