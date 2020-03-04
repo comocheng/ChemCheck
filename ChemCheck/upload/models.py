4d238952087617ea8b1b44c0b7f3d5c19f4a56a2
12Chao <xu.chao@husky.neu.edu>
2020-03-04 12:07:49 -0500

    add check polynomial discontinuity function

--- ChemCheck/upload/models.py
+++ ChemCheck/upload/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+import yaml
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 
 # Create your models here.
 
 def upload_to(instance, filename):
     return 'uploads/{id}/{fn}'.format(id=instance.pk,fn=filename)
+
 
 class Mechanism(models.Model):
     """
@@ -26,6 +33,7 @@ class Mechanism(models.Model):
 
     def get_absolute_url(self):
         return 'uploads/{self.id}/'
+    
 
 
     def save(self, *args, **kwargs):
@@ -52,3 +60,121 @@ class Mechanism(models.Model):
                 setattr(self, name, val)
         # Save the model, with all the files
         super(self.__class__, self).save(*args, **kwargs)
+
+# class Images(models.Model):
+#     """
+#     Images with polynomial discontinuities
+#     """
+#     mechanism = models.ForeignKey(Mechanism, on_delete=models.CASCADE)
+#     discontinuous_species = models.ImageField(upload_to=upload_to, verbose_name='discontinuous_species_images')
+
+def cp_calculate(T, T_mid, Nasa_poly_low, Nasa_poly_high):
+    if T < T_mid:
+        cp_low = Nasa_poly_low[0] + Nasa_poly_low[1] * T + Nasa_poly_low[2] * T ** 2 + Nasa_poly_low[3] * T ** 3 + Nasa_poly_low[4] * T ** 4
+        return cp_low
+    elif T > T_mid:
+        cp_high = Nasa_poly_high[0] + Nasa_poly_high[1] * T + Nasa_poly_high[2] * T ** 2 + Nasa_poly_high[3] * T ** 3 + Nasa_poly_high[4] * T ** 4
+        return cp_high
+    else:
+        cp_low = Nasa_poly_low[0] + Nasa_poly_low[1] * T + Nasa_poly_low[2] * T ** 2 + Nasa_poly_low[3] * T ** 3 + Nasa_poly_low[4] * T ** 4
+        cp_high = Nasa_poly_high[0] + Nasa_poly_high[1] * T + Nasa_poly_high[2] * T ** 2 + Nasa_poly_high[3] * T ** 3 + Nasa_poly_high[4] * T ** 4
+        if cp_low - cp_high == 0:
+            return cp_low
+        else:
+            return cp_low, cp_high
+
+def h_calculate(T, T_mid, Nasa_poly_low, Nasa_poly_high):
+    if T < T_mid:
+       h_low = Nasa_poly_low[0] + Nasa_poly_low[1] / 2 * T + Nasa_poly_low[2] / 3 * T ** 2 + Nasa_poly_low[3] / 4 * T ** 3 + Nasa_poly_low[4] / 5 * T ** 4 + Nasa_poly_low[5] / T
+       return h_low
+    elif T > T_mid:
+        h_high = Nasa_poly_high[0] + Nasa_poly_high[1] / 2 * T + Nasa_poly_high[2] / 3 * T ** 2 + Nasa_poly_high[3] / 4 * T ** 3 + Nasa_poly_high[4] / 5 * T ** 4 + Nasa_poly_high[5] / T
+        return h_high
+    else:
+        h_low = Nasa_poly_low[0] + Nasa_poly_low[1] / 2 * T + Nasa_poly_low[2] / 3 * T ** 2 + Nasa_poly_low[3] / 4 * T ** 3 + Nasa_poly_low[4] / 5 * T ** 4 + Nasa_poly_low[5] / T
+        h_high = Nasa_poly_high[0] + Nasa_poly_high[1] / 2 * T + Nasa_poly_high[2] / 3 * T ** 2 + Nasa_poly_high[3] / 4 * T ** 3 + Nasa_poly_high[4] / 5 * T ** 4 + Nasa_poly_high[5] / T
+        if h_low - h_high == 0:
+            return h_low
+        else:
+            return h_low, h_high
+
+def s_calculate(T, T_mid, Nasa_poly_low, Nasa_poly_high):
+    if T < T_mid:
+       s_low = Nasa_poly_low[0] * math.log(T) + Nasa_poly_low[1] * T + Nasa_poly_low[2] / 2 * T ** 2 + Nasa_poly_low[3] / 3 * T ** 3 + Nasa_poly_low[4] / 4 * T ** 4 + Nasa_poly_low[6]
+       return s_low
+    elif T > T_mid:
+        s_high = Nasa_poly_high[0] * math.log(T) + Nasa_poly_high[1] * T + Nasa_poly_high[2] / 2 * T ** 2 + Nasa_poly_high[3] / 3 * T ** 3 + Nasa_poly_high[4] / 4 * T ** 4 + Nasa_poly_high[6]
+        return s_high
+    else:
+        s_low = Nasa_poly_low[0] * math.log(T) + Nasa_poly_low[1] * T + Nasa_poly_low[2] / 2 * T ** 2 + Nasa_poly_low[3] / 3 * T ** 3 + Nasa_poly_low[4] / 4 * T ** 4 + Nasa_poly_low[6]
+        s_high = Nasa_poly_high[0] * math.log(T) + Nasa_poly_high[1] * T + Nasa_poly_high[2] / 2 * T ** 2 + Nasa_poly_high[3] / 3 * T ** 3 + Nasa_poly_high[4] / 4 * T ** 4 + Nasa_poly_high[6]
+        if s_low - s_high == 0:
+            return s_low
+        else:
+            return s_low, s_high
+
+class ChemError:
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+        
+    
+    def check_continuity(self):
+        discontinuous_species = []
+        with open(self.path, 'r') as f:
+            chem_data = yaml.load(f, Loader=yaml.FullLoader)
+        for species in chem_data['species']:
+            #T_low = species['thermo']['temperature-ranges'][0]
+            T_mid = species['thermo']['temperature-ranges'][1]
+            #T_high = species['thermo']['temperature-ranges'][2]
+            Nasa_poly_high = species['thermo']['data'][1]
+            Nasa_poly_low = species['thermo']['data'][0]
+            cp_low = Nasa_poly_low[0] + Nasa_poly_low[1] * T_mid + Nasa_poly_low[2] * T_mid ** 2 + Nasa_poly_low[3] * T_mid ** 3 + Nasa_poly_low[4] * T_mid ** 4
+            cp_high = Nasa_poly_high[0] + Nasa_poly_high[1] * T_mid + Nasa_poly_high[2] * T_mid ** 2 + Nasa_poly_high[3] * T_mid ** 3 + Nasa_poly_high[4] * T_mid ** 4
+            h_low = Nasa_poly_low[0] + Nasa_poly_low[1] / 2 * T_mid + Nasa_poly_low[2] / 3 * T_mid ** 2 + Nasa_poly_low[3] / 4 * T_mid ** 3 + Nasa_poly_low[4] / 5 * T_mid ** 4 + Nasa_poly_low[5] / T_mid
+            h_high = Nasa_poly_high[0] + Nasa_poly_high[1] / 2 * T_mid + Nasa_poly_high[2] / 3 * T_mid ** 2 + Nasa_poly_high[3] / 4 * T_mid ** 3 + Nasa_poly_high[4] / 5 * T_mid ** 4 + Nasa_poly_high[5] / T_mid
+            s_low = Nasa_poly_low[0] * math.log(T_mid) + Nasa_poly_low[1] * T_mid + Nasa_poly_low[2] / 2 * T_mid ** 2 + Nasa_poly_low[3] / 3 * T_mid ** 3 + Nasa_poly_low[4] / 4 * T_mid ** 4 + Nasa_poly_low[6]
+            s_high = Nasa_poly_high[0] * math.log(T_mid) + Nasa_poly_high[1] * T_mid + Nasa_poly_high[2] / 2 * T_mid ** 2 + Nasa_poly_high[3] / 3 * T_mid ** 3 + Nasa_poly_high[4] / 4 * T_mid ** 4 + Nasa_poly_high[6]
+            if abs(cp_high - cp_low) <= 0.1 and abs(h_low - h_high) <= 0.1 and abs(s_low - s_high) <= 0.1:
+                pass
+            else:
+                image = '{}.png'.format(species['name'])
+                img_path = os.path.join(os.path.split(self.path)[0], image)
+                img_url = img_path.split('ChemCheck')[2]
+                discontinuous_species.append(img_url)
+                #gas = ct.Solution(self.path)
+                #sp = gas.species(species['name'])
+                T = np.linspace(500, 3000, 200)
+                fig,ax = plt.subplots(1,3,figsize=(8,3.5))
+                cp = [cp_calculate(tt, T_mid, Nasa_poly_low, Nasa_poly_high) for tt in T]
+                h = [h_calculate(tt, T_mid, Nasa_poly_low, Nasa_poly_high) for tt in T]
+                s = [s_calculate(tt, T_mid, Nasa_poly_low, Nasa_poly_high) for tt in T]
+                ax[0].plot(T,cp)
+                ax[0].set_title('$c_p/R$')
+                ax[1].plot(T,h)
+                ax[1].set_title('$h/RT$')
+                ax[2].plot(T,s)
+                ax[2].set_title('$s/R$')
+                fig.tight_layout()
+                fig.suptitle('{} thermo property'.format(species['name']), y=1)
+                fig.savefig(img_path)
+        return discontinuous_species
+
+
+                # T_mid = symbols('x')
+                # cp_low = Nasa_poly_low[0] + Nasa_poly_low[1] * T_mid + Nasa_poly_low[2] * T_mid ** 2 + Nasa_poly_low[3] * T_mid ** 3 + Nasa_poly_low[4] * T_mid ** 4
+                # cp_high = Nasa_poly_high[0] + Nasa_poly_high[1] * T_mid + Nasa_poly_high[2] * T_mid ** 2 + Nasa_poly_high[3] * T_mid ** 3 + Nasa_poly_high[4] * T_mid ** 4
+                # h_low = Nasa_poly_low[0] + Nasa_poly_low[1] / 2 * T_mid + Nasa_poly_low[2] / 3 * T_mid ** 2 + Nasa_poly_low[3] / 4 * T_mid ** 3 + Nasa_poly_low[4] / 5 * T_mid ** 4 + Nasa_poly_low[5] / T_mid
+                # h_high = Nasa_poly_high[0] + Nasa_poly_high[1] / 2 * T_mid + Nasa_poly_high[2] / 3 * T_mid ** 2 + Nasa_poly_high[3] / 4 * T_mid ** 3 + Nasa_poly_high[4] / 5 * T_mid ** 4 + Nasa_poly_high[5] / T_mid
+                # s_low = Nasa_poly_low[0] * ln(T_mid) + Nasa_poly_low[1] * T_mid + Nasa_poly_low[2] / 2 * T_mid ** 2 + Nasa_poly_low[3] / 3 * T_mid ** 3 + Nasa_poly_low[4] / 4 * T_mid ** 4 + Nasa_poly_low[6]
+                # s_high = Nasa_poly_high[0] * ln(T_mid) + Nasa_poly_high[1] * T_mid + Nasa_poly_high[2] / 2 * T_mid ** 2 + Nasa_poly_high[3] / 3 * T_mid ** 3 + Nasa_poly_high[4] / 4 * T_mid ** 4 + Nasa_poly_high[6]
+                # T_cp = solveset(Abs(cp_high - cp_low) < 1, T_mid, domain=S.Reals)
+                # T_h = solveset(Abs(h_high - h_low) < 1, T_mid, domain=S.Reals)
+                # T_s = solveset(Abs(s_high - s_low) < 1, T_mid, domain=S.Reals)
+                # T_range = Interval(T_low, T_high)
+                # T_continuous = T_cp.intersect(T_h).intersect(T_s).intersect(T_range)
+                # T_continuouse = str(T_continuous)[str(T_continuous).find('Interval'):len(str(T_continuous))]
+                # if not T_continuouse:
+                #     print(species['name'])
+                # else:
+                #     print(species['name'], T_continuouse)
--- ChemCheck/upload/urls.py
+++ ChemCheck/upload/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('mechanism/<int:pk>/delete_transport', views.MechanismtransportDeleteView.as_view(), name='delete_file_transport'),
     path('mechanism/<int:pk>/delete_surface', views.MechanismsurfaceDeleteView.as_view(), name='delete_file_surface'),
     path('mechanism/<int:pk>/update', views.MechanismUpdateView.as_view(), name='update_file_transport'),
+    path('mechanism/<int:pk>/chemcheck', views.chemcheck, name='chemcheck'),
 ]
