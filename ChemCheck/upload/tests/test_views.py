59f58f70c21d8ce41d44dbe6e3b3057777024f7b
12Chao <xu.chao@husky.neu.edu>
2020-05-03 18:47:43 -0400

    add views test for  discontinuity, negativeA, collision violation check

--- ChemCheck/upload/tests/test_views.py
+++ ChemCheck/upload/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.uploadedfile import SimpleUploadedFile, UploadedFile
 from upload.models import Mechanism
 import os
 from canteradebugger.settings import MEDIA_ROOT
@@ -19,7 +19,7 @@ class TestViews(TestCase):
         fake_file = SimpleUploadedFile(
             'test-file.txt',
             'this is what\'s in the file that isn\'t a file'.encode(),
-            content_type='test/plain')
+            content_type='text/plain')
         post_response = self.client.post(reverse('upload'),{
         'ck_mechanism_file':fake_file
         })
@@ -65,3 +65,42 @@ class TestViews(TestCase):
         self.assertEquals(delete_post_response.status_code, 302)
         self.assertFalse(Mechanism.objects.get(id=1).ck_mechanism_file)
         self.assertFalse(os.path.isfile(str(MEDIA_ROOT)+'/uploads/1/update-file.txt'))
+
+class TestCheckViews(TestCase):
+    def SetUp(self):
+        self.client = Client()
+    
+    def test_chemcheck_view(self):
+        file_dir = os.path.join(os.getcwd(), 'upload/tests/cantera.yaml')
+        with open(file_dir, 'rb') as f:
+            test_file = SimpleUploadedFile(
+            'cantera.yaml',
+            f.read(),
+            content_type='text/yaml')
+        mech = Mechanism.objects.create(
+            id=2,
+            ct_mechanism_file=test_file
+            )
+        chemcheck_get_response = self.client.get(reverse('chemcheck', args=[2]))
+        self.assertEqual(chemcheck_get_response.status_code, 200)
+    
+    # start of pdep negative A test
+        get_pdep_neg_A_response = self.client.get(reverse('check_pdep_negative_A', args=[2]))
+        self.assertEqual(get_pdep_neg_A_response.status_code, 200)
+       
+    
+    # start of duplicate reaction negative A test
+        get_dup_neg_A_response = self.client.get(reverse('check_negative_dup_rxns_negative_A', args=[2]))
+        self.assertEqual(get_dup_neg_A_response.status_code, 200)
+    
+    # start of reaction condition test
+        get_rxn_condition_response = self.client.get(reverse('reaction_condition', args=[2]))
+        self.assertEqual(get_rxn_condition_response.status_code, 200)
+    
+    # start of collision violation check test
+        get_collision_check_response = self.client.get(reverse('collision_violation', args=[2]))
+        self.assertEqual(get_collision_check_response.status_code, 200)
+        path_mech = mech.ct_mechanism_file.path
+        os.remove(path_mech)
+        if len(os.listdir(os.path.join(MEDIA_ROOT,'uploads/2/'))) == 0:
+            os.rmdir(os.path.join(MEDIA_ROOT,'uploads/2/'))
